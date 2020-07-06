b2e66b84d97271ecced0ea14ae185a55b1e4dcd9
AlfieriChou <alfierichou@gmail.com>
2020-07-07 05:10:10 +0800

    refactor: generator swagger openapi

--- framework/buildSwaggerDoc.js
+++ framework/buildSwaggerDoc.js
@@ -5,109 +5,107 @@ const resTypeList = ['array', 'object', 'number', 'string', 'html']
 
 const buildSwaggerDoc = async (info, ctx) => {
   const { schemas, remoteMethods } = ctx
-  const methods = await Object.entries(remoteMethods)
-    .reduce(async (promise, [schemaKey, {
-      path, method, tags, summary, query, params, requestBody, output
-    }]) => {
-      assert(path, `${schemaKey} path is required`)
-      assert(method, `${schemaKey} method is required`)
-      const methodRet = await promise
-      const content = {
-        tags: tags || ['default'],
-        summary: summary || ''
-      }
-      if (query || params) {
-        content.parameters = Object.entries(query || params)
-          .reduce((ret, [propKey, propValue]) => (
-            [...ret, {
-              name: propKey,
-              in: query ? 'query' : 'path',
-              description: propValue.description,
-              schema: {
-                type: propValue.type
-              },
-              required: !query
-            }]
-          ), [])
-      }
-      if (requestBody) {
-        content.requestBody = {
-          required: true,
-          content: {
-            'application/json': {
-              schema: {
-                type: 'object',
-                properties: requestBody.body,
-                required: requestBody.required
-              }
+  const methods = Object.entries(remoteMethods).map(([schemaKey, {
+    path, method, tags, summary, query, params, requestBody, output
+  }]) => {
+    assert(path, `${schemaKey} path is required`)
+    assert(method, `${schemaKey} method is required`)
+    const content = {
+      tags: tags || ['default'],
+      summary: summary || '',
+      parameters: [
+        ...(Object.entries(query || {}).map(([propKey, propValue]) => ({
+          name: propKey,
+          in: 'query',
+          description: propValue.description,
+          schema: {
+            type: propValue.type
+          },
+          required: false
+        }
+        ))),
+        ...(Object.entries(params || {}).map(([propKey, propValue]) => ({
+          name: propKey,
+          in: 'path',
+          description: propValue.description,
+          schema: {
+            type: propValue.type
+          },
+          required: true
+        }
+        )))],
+      requestBody: requestBody ? {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: requestBody.body,
+              required: requestBody.required
             }
           }
         }
-      }
-      if (output) {
-        content.responses = await Object.entries(output)
-          .reduce(async (resPromise, [responseKey, { type, result }]) => {
-            const outputDatas = await resPromise
-            if (!resTypeList.includes(type)) throw new Error('output type mast ba array or object or number or string or html!')
-            if (type === 'html') {
-              return {
-                ...outputDatas,
-                [responseKey]: {
-                  description: 'response success',
-                  content: {
-                    'text/html': {}
-                  }
-                }
-              }
-            }
-            outputDatas[200] = {
-              description: 'response success',
-              content: {
-                'application/json': {
-                  schema: { $ref: `#/components/schemas/${schemaKey.split('-')[0]}` }
-                }
-              }
-            }
-            let outputSchema
-            if (type === 'array') {
-              outputSchema = {
-                type: 'array',
-                items: result || {
-                  type: 'string'
-                }
-              }
-            }
-            if (type === 'object') {
-              outputSchema = { type: 'object', properties: result || {} }
-            }
-            if (type === 'number') {
-              outputSchema = { type: 'object', properties: { result: { type: 'number', description: '返回标识' } } }
-            }
-            if (type === 'string') {
-              outputSchema = { type: 'object', properties: { result: { type: 'string', description: '返回标识' } } }
-            }
+      } : undefined
+    }
+    if (output) {
+      content.responses = Object.entries(output)
+        .reduce((acc, [responseKey, { type, result }]) => {
+          if (!resTypeList.includes(type)) throw new Error('output type mast ba array or object or number or string or html!')
+          if (type === 'html') {
             return {
-              ...outputDatas,
+              ...acc,
               [responseKey]: {
                 description: 'response success',
                 content: {
-                  'application/json': {
-                    schema: outputSchema
-                  }
+                  'text/html': {}
                 }
               }
             }
-          }, Promise.resolve({}))
-      }
-      return [
-        ...methodRet,
-        {
-          [path]: {
-            [method]: content
           }
-        }
-      ]
-    }, Promise.resolve([]))
+          acc[200] = {
+            description: 'response success',
+            content: {
+              'application/json': {
+                schema: { $ref: `#/components/schemas/${schemaKey.split('-')[0]}` }
+              }
+            }
+          }
+          const outputSchema = {}
+          if (type === 'array') {
+            outputSchema.type = 'array'
+            outputSchema.items = result || {}
+          }
+          if (type === 'object') {
+            outputSchema.type = 'object'
+            outputSchema.properties = result || {}
+          }
+          if (type === 'number') {
+            outputSchema.type = 'number'
+            outputSchema.description = '返回标识'
+          }
+          if (type === 'string') {
+            outputSchema.type = 'number'
+            outputSchema.description = '返回标识'
+          }
+          return {
+            ...acc,
+            [responseKey]: {
+              description: 'response success',
+              content: {
+                'application/json': {
+                  schema: outputSchema
+                }
+              }
+            }
+          }
+        }, {})
+    }
+    return {
+      [path]: {
+        [method]: content
+      }
+    }
+  })
   return {
     openapi: '3.0.0',
     info,
