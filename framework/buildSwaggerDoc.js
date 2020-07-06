4a7640f952346d6e981c59a162ac5e3260e8f195
AlfieriChou <alfierichou@gmail.com>
2020-07-07 05:20:13 +0800

    refactor: generator swagger openapi

--- framework/buildSwaggerDoc.js
+++ framework/buildSwaggerDoc.js
@@ -1,68 +1,92 @@
 const _ = require('lodash')
-const assert = require('assert')
 
 const resTypeList = ['array', 'object', 'number', 'string', 'html']
 
-const buildSwaggerDoc = async (info, ctx) => {
-  const { schemas, remoteMethods } = ctx
+const buildSwaggerDoc = async (info, { remoteMethods, schemas }) => {
   const methods = Object.entries(remoteMethods).map(([schemaKey, {
     path, method, tags, summary, query, params, requestBody, output
-  }]) => {
-    assert(path, `${schemaKey} path is required`)
-    assert(method, `${schemaKey} method is required`)
-    const content = {
-      tags: tags || ['default'],
-      summary: summary || '',
-      parameters: [
-        ...(Object.entries(query || {}).map(([propKey, propValue]) => ({
-          name: propKey,
-          in: 'query',
-          description: propValue.description,
-          schema: {
-            type: propValue.type
-          },
-          required: false
-        }
-        ))),
-        ...(Object.entries(params || {}).map(([propKey, propValue]) => ({
-          name: propKey,
-          in: 'path',
-          description: propValue.description,
-          schema: {
-            type: propValue.type
-          },
-          required: true
-        }
-        )))],
-      requestBody: requestBody ? {
-        required: true,
-        content: {
-          'application/json': {
+  }]) => ({
+    [path]: {
+      [method]: {
+        tags: tags || ['default'],
+        summary: summary || '',
+        parameters: [
+          ...(Object.entries(query || {}).map(([propKey, propValue]) => ({
+            name: propKey,
+            in: 'query',
+            description: propValue.description,
             schema: {
-              type: 'object',
-              properties: requestBody.body,
-              required: requestBody.required
+              type: propValue.type
+            },
+            required: false
+          }
+          ))),
+          ...(Object.entries(params || {}).map(([propKey, propValue]) => ({
+            name: propKey,
+            in: 'path',
+            description: propValue.description,
+            schema: {
+              type: propValue.type
+            },
+            required: true
+          }
+          )))],
+        requestBody: requestBody ? {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: requestBody.body,
+                required: requestBody.required
+              }
             }
           }
-        }
-      } : undefined
-    }
-    if (output) {
-      content.responses = Object.entries(output)
-        .reduce((acc, [responseKey, { type, result }]) => {
-          if (!resTypeList.includes(type)) throw new Error('output type mast ba array or object or number or string or html!')
-          if (type === 'html') {
+        } : undefined,
+        responses: output ? Object.entries(output)
+          .reduce((acc, [responseKey, { type, result }]) => {
+            if (!resTypeList.includes(type)) throw new Error('output type mast ba array or object or number or string or html!')
+            if (type === 'html') {
+              return {
+                ...acc,
+                [responseKey]: {
+                  description: 'response success',
+                  content: {
+                    'text/html': {}
+                  }
+                }
+              }
+            }
+            const outputSchema = {}
+            if (type === 'array') {
+              outputSchema.type = 'array'
+              outputSchema.items = result || {}
+            }
+            if (type === 'object') {
+              outputSchema.type = 'object'
+              outputSchema.properties = result || {}
+            }
+            if (type === 'number') {
+              outputSchema.type = 'number'
+              outputSchema.description = '返回标识'
+            }
+            if (type === 'string') {
+              outputSchema.type = 'number'
+              outputSchema.description = '返回标识'
+            }
             return {
               ...acc,
               [responseKey]: {
                 description: 'response success',
                 content: {
-                  'text/html': {}
+                  'application/json': {
+                    schema: outputSchema
+                  }
                 }
               }
             }
-          }
-          acc[200] = {
+          }, {}) : {
+          200: {
             description: 'response success',
             content: {
               'application/json': {
@@ -70,42 +94,10 @@ const buildSwaggerDoc = async (info, ctx) => {
               }
             }
           }
-          const outputSchema = {}
-          if (type === 'array') {
-            outputSchema.type = 'array'
-            outputSchema.items = result || {}
-          }
-          if (type === 'object') {
-            outputSchema.type = 'object'
-            outputSchema.properties = result || {}
-          }
-          if (type === 'number') {
-            outputSchema.type = 'number'
-            outputSchema.description = '返回标识'
-          }
-          if (type === 'string') {
-            outputSchema.type = 'number'
-            outputSchema.description = '返回标识'
-          }
-          return {
-            ...acc,
-            [responseKey]: {
-              description: 'response success',
-              content: {
-                'application/json': {
-                  schema: outputSchema
-                }
-              }
-            }
-          }
-        }, {})
-    }
-    return {
-      [path]: {
-        [method]: content
+        }
       }
     }
-  })
+  }))
   return {
     openapi: '3.0.0',
     info,
